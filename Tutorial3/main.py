3a243592a3b39e46e7ad0c8de76d66654ba70f1f
Kurtis Odom <kurtis.odom@accelirate.com>
2020-09-04 16:26:11 -0400

    Implemented GUI with screen manager

--- Tutorial3/main.py
+++ Tutorial3/main.py
@@ -1,10 +1,25 @@
-import kivy
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class MyApp(App):
+# Login screen
+class MainWindow(Screen):
+    pass
+
+# Landing screen
+class SecondWindow(Screen):
+    pass
+
+# Screen manager
+class WindowManager(ScreenManager):
+    pass
+
+# Build from whatever kv file specified
+kv = Builder.load_file("my.kv")
+
+class MyMainApp(App):
     def build(self):
-        return
+        return kv
 
 if __name__ == "__main__":
-    MyApp().run()
-    
+    MyMainApp().run()
